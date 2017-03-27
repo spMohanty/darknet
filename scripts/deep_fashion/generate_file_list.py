e5c4225ef229ba44ee7ba439a4371be78711c9f5
S.P. Mohanty <spmohanty91@gmail.com>
2017-03-27 07:31:25 +0200

    Update generate filelist

--- scripts/deep_fashion/generate_file_list.py
+++ scripts/deep_fashion/generate_file_list.py
@@ -3,7 +3,9 @@
 import glob
 import os
 
-files = glob.glob("images/*/*")
+_parent = "../../../../DeepFashion/"
+
+files = glob.glob(_parent+"images/*/*")
 import random
 
 random.shuffle(files)
@@ -13,7 +15,7 @@ files = [os.path.abspath(x) for x in files]
 percent_train = 0.95
 train_length = int(0.95*len(files))
 
-open("filelist_train.txt", "w").write("\n".join(files[:train_length]))
-open("filelist_val.txt", "w").write("\n".join(files[train_length:]))
+open(_parent+"filelist_train.txt", "w").write("\n".join(files[:train_length]))
+open(_parent+"filelist_val.txt", "w").write("\n".join(files[train_length:]))
 
 #root@iccluster043:/mount/SDF/YOLO/darknet$ ./darknet yolo train cfg/deep_fashion.cfg ../../DeepFashion/darknet_weights/extraction.conv.weights -c_fl_train ../../DeepFashion/filelist_train.txt -c_dir_backup ../snapshots/ -c_classes ../../DeepFashion/classes.txt
